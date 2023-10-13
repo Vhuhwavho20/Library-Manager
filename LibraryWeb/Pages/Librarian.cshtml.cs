26cd0812b4a29329ddb0e200a0acb425ca5cd5c0
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2023-10-13 20:08:20 +0200

    Added Librarian Page

---
+++ LibraryWeb/Pages/Librarian.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryWeb.Pages
+{
+    public class LibrarianModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
