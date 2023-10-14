c77577a6c12dd886a8c0af8110376e57b217149d
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2023-10-14 18:07:08 +0200

    Implemented LibrarianServices

--- LibraryWeb/Program.cs
+++ LibraryWeb/Program.cs
@@ -1,7 +1,16 @@
+using LibraryWeb.Data;
+using LibraryWeb.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<LibrarianContext>(options =>
+    options.UseSqlite("Data Source=Librarian.db"));
+builder.Services.AddScoped<LibrarianService>();
 
 var app = builder.Build();
 
---
+++ LibraryWeb/Services/LibrarianServices.cs
@@ -0,0 +1,47 @@
+using LibraryWeb.Data;
+using LibraryWeb.Models;
+
+
+namespace LibraryWeb.Services
+{
+public class LibrarianService
+    {
+        private readonly LibrarianContext _context = default!;
+
+        public LibrarianService(LibrarianContext context)
+        {
+            _context = context;
+        }
+        
+        public IList<Librarian> GetLibrarians()
+        {
+            if(_context.Librarians != null)
+            {
+                return _context.Librarians.ToList();
+            }
+            return new List<Librarian>();
+        }
+
+        public void AddLibrarian(Librarian librarian)
+        {
+            if (_context.Librarians != null)
+            {
+                _context.Librarians.Add(librarian);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeletePizza(int id)
+        {
+            if (_context.Librarians != null)
+            {
+                var librarian = _context.Librarians.Find(id);
+                if (librarian != null)
+                {
+                    _context.Librarians.Remove(librarian);
+                    _context.SaveChanges();
+                }
+            }
+        }
+    }
+}
