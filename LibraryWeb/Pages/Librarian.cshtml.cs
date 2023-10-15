a692acfdf7ae7e610f1bd2094157c3dd572ba233
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2023-10-15 13:36:37 +0200

    Implemented log in authentication link to database

--- LibraryWeb/Pages/Librarian.cshtml.cs
+++ LibraryWeb/Pages/Librarian.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using LibraryWeb.Models;
 using LibraryWeb.Services;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LibraryWeb.Pages
 {
@@ -9,11 +10,32 @@ namespace LibraryWeb.Pages
     {
         private readonly LibrarianService _service;
         public IList<Librarian> LibrarianList { get;set; } = default!;
+        [BindProperty]
+        public string name { get; set; } = default!;
+        [BindProperty]
+        public string pw { get; set; } = default!;
 
         public LibrarianModel(LibrarianService service)
         {
         _service = service;
     }
+    
+    public IActionResult OnPost()
+{
+    if (pw == null || name==null)
+    {
+        return Page();
+    }
+    List<Librarian> librarians = _service.GetLibrarians().ToList();
+    for(int i = 0; i < librarians.Count; i++){
+        if(name==librarians[i].fullname){
+            Console.WriteLine("Log In successful");
+        }
+    }
+
+    
+    return Redirect("https://example.com");
+}
 
         public void OnGet()
         {
