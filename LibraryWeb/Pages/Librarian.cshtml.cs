0f6d7fba6f5bee0386515b087dabd98453f8271b
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2023-10-15 12:03:21 +0200

    Updated build

--- LibraryWeb/Pages/Librarian.cshtml.cs
+++ LibraryWeb/Pages/Librarian.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LibraryWeb.Models;
+using LibraryWeb.Services;
 
 namespace LibraryWeb.Pages
 {
     public class LibrarianModel : PageModel
     {
+        private readonly LibrarianService _service;
+        public IList<Librarian> LibrarianList { get;set; } = default!;
+
+        public LibrarianModel(LibrarianService service)
+        {
+        _service = service;
+    }
+
         public void OnGet()
         {
+            LibrarianList = _service.GetLibrarians();
         }
     }
 }
