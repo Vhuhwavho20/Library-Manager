61aefae12885551bc62e355cd4c0dadf547e3f79
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2023-10-16 12:16:29 +0200

    Implemented Book model

---
+++ LibraryWeb/Models/Book.cs
@@ -0,0 +1,13 @@
+namespace LibraryWeb.Models;
+
+public class Book{
+
+public int Id{get;set;}
+
+public string? book_title{get;set;}
+
+public string? book_author{get;set;}
+
+public string? status{get;set;}
+
+}
