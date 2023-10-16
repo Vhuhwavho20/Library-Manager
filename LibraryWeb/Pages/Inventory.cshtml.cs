75aa43f5c3ae57fddab7a4adb9a0cf588645a03a
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2023-10-16 12:56:28 +0200

    Implemented Books controller for Inventory Page

--- LibraryWeb/Pages/Inventory.cshtml.cs
+++ LibraryWeb/Pages/Inventory.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LibraryWeb.Models;
+using LibraryWeb.Services;
 
 namespace LibraryWeb.Pages
 {
     public class InventoryModel : PageModel
     {
+    private readonly LibrarianService _service;
+    public IList<Book> BookList { get;set; } = default!;
+
+    public InventoryModel(LibrarianService service){
+        _service = service;
+    }
         public void OnGet()
         {
+            BookList = _service.GetBooks();
         }
     }
 }
