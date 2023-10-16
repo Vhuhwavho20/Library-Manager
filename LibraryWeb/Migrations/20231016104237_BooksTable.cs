bb0cf3c9a393f2e34a04bf4c6dd020d211d168c3
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2023-10-16 12:43:19 +0200

    Added Books to Migrations

---
+++ LibraryWeb/Migrations/20231016104237_BooksTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class BooksTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    book_title = table.Column<string>(type: "TEXT", nullable: true),
+                    book_author = table.Column<string>(type: "TEXT", nullable: true),
+                    status = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
