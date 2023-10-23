45acb33914a7db505a76ec266673b8c1ef2bdc85
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2023-10-23 17:27:04 +0200

    Implemented redirect to message with borrower

--- LibraryWeb/Pages/Communication.cshtml.cs
+++ LibraryWeb/Pages/Communication.cshtml.cs
@@ -11,9 +11,41 @@ namespace LibraryWeb.Pages
 
         public IList<Borrower> BorrowerList { get;set; } = default!;
 
+        
+        [BindProperty]
+        public string searchquery{get; set;} = default!;
+
         public CommunicationModel(LibrarianService service){
         _service = service;
     }
+
+    public IActionResult OnPost(){
+        if(searchquery==null){
+            return Page();
+        }
+        List<Borrower> borrowers = _service.GetBorrowers().ToList();
+    List<Borrower> searchresult = new List<Borrower>();
+    for(int i = 0; i< borrowers.Count;i++){
+        string? borrower_in_library = borrowers[i].fullname;
+        if(borrower_in_library?.ToLower().IndexOf(searchquery.ToLower())!=-1){
+            searchresult.Add(borrowers[i]);
+        }
+    }
+    
+    BorrowerList = searchresult;
+
+
+
+
+
+        return Page();
+    }
+
+    public IActionResult Redirect_Click()
+{
+    return RedirectToPage("/Message");
+}
+
         public void OnGet()
         {
             BorrowerList = _service.GetBorrowers();
